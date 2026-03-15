cb1636f2ae708e87f3e0df6fc4e2a350a360ba76
irisbarney damasceno martins filho <irisbarney.iy@gmail.com>
2026-03-15 10:26:28 -0300

    versão torta preferida

--- formulario.py
+++ formulario.py
@@ -1,7 +1,17 @@
 nome = input("Digite seu nome: ")
-idade = int(input("Diite sua idade: "))
+idade = int(input("Digite sua idade: "))
 
 if idade >= 18:
-    print(f"Olá {nome}, você tem a idade suficiente.")
+    print(f"Olá {nome}, você tem a idade suficiente.\n\n")
+    preferido = int(input("[1] Torta de limão \n[2] Torta de morango \n[3] Torta de abacaxi \n\nQual você prefere: "))
+    if preferido == 1:
+        print(f"Legal {nome}, você gosta de Torta de limão")
+    elif preferido == 2:
+        print(f"Legal {nome}, você gosta de Torta de morango")
+    elif preferido == 3:
+        print(f"Legal {nome}, você gosta de Torta de abacaxi")
+    else: print(f"Opção inválida {nome}")
+        
 else: 
     print(f"Olá {nome}, você não tem a idade suficiente para acessar.")
+
